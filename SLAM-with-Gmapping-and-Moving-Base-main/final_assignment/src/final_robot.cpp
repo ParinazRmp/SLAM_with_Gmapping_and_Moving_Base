e740510271ca718724ed018e4ec671247d3fb9f4
Parinaz <94115975+ParinazRmp@users.noreply.github.com>
2023-06-29 01:57:32 +0200

    Update final_robot.cpp

--- SLAM-with-Gmapping-and-Moving-Base-main/final_assignment/src/final_robot.cpp
+++ SLAM-with-Gmapping-and-Moving-Base-main/final_assignment/src/final_robot.cpp
@@ -1,3 +1,20 @@
+/**
+ * Robotics control in a simulated environment
+ * Robotics Engineering
+ * @file final_robot.cpp
+ *
+ * @brief SLAM with Gmapping and Moving Base
+ *
+ * This file contains the implementation of a simulation of a robot using ROS (Robot Operating System)
+ * with advanced capabilities like simultaneous localization and mapping (SLAM) and autonomous movement.
+ * The robot is equipped with laser scanners for obstacle detection and can be controlled through various
+ * interfaces, including user input, keyboard control, and driving assistance.
+ *
+ * @author Parinaz
+ * @version 1.0
+ * @date 01/06/2023
+ */
+
 #include <iostream>
 #include <string>
 #include <chrono>
@@ -8,176 +25,187 @@
 #include "move_base_msgs/MoveBaseActionGoal.h"
 #include "move_base_msgs/MoveBaseActionFeedback.h"
 
+
 // Declare the publishers
-ros::Publisher pub_goal;
-ros::Publisher pub_canc;
-ros::Publisher pub_vel;
+ros::Publisher pub_goal; /**< Publisher for sending goal positions */
+ros::Publisher pub_canc; /**< Publisher for canceling the current goal */
+ros::Publisher pub_vel;  /**< Publisher for sending robot velocities */
 
 // Global variables
-float lin_vel = 0.0; // Robot linear velocity 
+float lin_vel = 0.0; // Robot linear velocity
 float ang_vel = 0.0; // Robot angular velocity
-char key; // User input from keyboard to drive the robot
+char key;           // User input from the keyboard to drive the robot
 
 int drive_flag = 0; // Enable/Disable driving assistance
-int time_flag = 0; // Compute the time elapsed since the request of the current goal
+int time_flag = 0;  // Compute the time elapsed since the request of the current goal
 
-int flag = 0; // Just to manage printing of the option Enable/Disable driving assistance
+int flag = 0;       // Just to manage printing of the option Enable/Disable driving assistance
 int print_flag = 0; // Just to manage printing in drivingAssistance
-int counter1 = 10; // Just to manage printing in manualDriving
-int counter2; // Just to manage printing in userInterface
+int counter1 = 10;  // Just to manage printing in manualDriving
+int counter2;       // Just to manage printing in userInterface
 
 float x_goal; // Current goal coordinate x
 float y_goal; // Current goal coordinate y
 
 std::string id = ""; // Goal ID
-std::chrono::high_resolution_clock::time_point t_start;  
-std::chrono::high_resolution_clock::time_point t_end; 
-
-#define DIST 0.35 // Minimum distance from the wall with the driving assistance enabled
-#define POS_ERROR 0.5 // Position range error
+std::chrono::high_resolution_clock::time_point t_start;
+std::chrono::high_resolution_clock::time_point t_end;
+ 
+#define DIST 0.35       // Minimum distance from the wall with the driving assistance enabled
+#define POS_ERROR 0.5   // Position range error
 #define MAX_TIME 120000000 // Maximum time to reach a goal (microseconds)
 
 /**
- * Function: provide an user interface to drive the robot autonomously.
- * A driving assistance can be enabled (or clearly disabled).
- * 
+ * @brief Provide a user interface to drive the robot autonomously.
+ *        A driving assistance can be enabled (or clearly disabled).
  */
+void manualDriving();
+
+/**
+ * @brief ROS Callback Function: check data from the robot's laser scanner and,
+ *        if the driving assistance is enabled, help the user to not crush
+ *        the robot against a wall.
+ * @param msg The laser scan message
+ */
+void drivingAssistance(const sensor_msgs::LaserScan::ConstPtr& msg);
+
+/**
+ * @brief ROS Callback Function: check if the robot is on the goal position and,
+ *        when there is a new goal, update the current Goal ID and save it in a
+ *        global variable.
+ * @param msg The action feedback message
+ */
+void currentStatus(const move_base_msgs::MoveBaseActionFeedback::ConstPtr& msg);
+
+/**
+ * @brief ROS Callback Function: check the current goal position and save its
+ *        module coordinates x and y in two global variables.
+ * @param msg The action goal message
+ */
+void currentGoal(const move_base_msgs::MoveBaseActionGoal::ConstPtr& msg);
+
+/**
+ * @brief Provide a user interface to choose the modality to drive the robot.
+ *        The options are:
+ *        1. Automatic driving (insert the coordinates to reach).
+ *           1.1 It is also possible to cancel the current goal.
+ *        2. Manual driving (without driving assistance).
+ *        3. Manual driving (with driving assistance);
+ *           3.1 The driving assistance can be simply enabled or disabled.
+ */
+void userInterface();
+
+/**
+ * @brief Main function of the program.
+ * @param argc Number of command-line arguments
+ * @param argv Array of command-line arguments
+ * @return 0 on successful execution
+ */
+int main(int argc, char **argv) {
+    // Initialize the ROS node
+    ros::init(argc, argv, "final_robot");
+    ros::NodeHandle nh;
+
+    // Define the publishers
+    pub_goal = nh.advertise<move_base_msgs::MoveBaseActionGoal>("move_base/goal", 1000);
+    pub_canc = nh.advertise<actionlib_msgs::GoalID>("move_base/cancel", 1000);
+    pub_vel = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
+
+    // Define the subscribers
+    ros::Subscriber sub_pos = nh.subscribe("/move_base/feedback", 1000, currentStatus); // Current Status feedback
+    ros::Subscriber sub_goal = nh.subscribe("/move_base/goal", 1000, currentGoal);     // Current Goal feedback
+    ros::Subscriber sub_laser = nh.subscribe("/scan", 1000, drivingAssistance);        // Laser scanner
+
+    // Multi-threading
+    ros::AsyncSpinner spinner(3);
+    spinner.start();
+    userInterface();
+    spinner.stop();
+    ros::shutdown();
+    ros::waitForShutdown();
+
+    return 0;
+}
+
 void manualDriving() {
-    // Local variables
+    // Define possible commands with corresponding actions
+    std::map<char, std::pair<float, float>> commandMap {
+        {'w', {lin_vel, 0}},         // Go forward
+        {'q', {lin_vel, ang_vel}},   // Curve left
+        {'s', {-lin_vel, 0}},        // Go backward
+        {'e', {lin_vel, -ang_vel}},  // Curve right
+        {'a', {0, ang_vel}},         // Turn left
+        {'d', {0, -ang_vel}},        // Turn right
+        {'r', {0, 0}}                // Emergency stop
+    };
+
     geometry_msgs::Twist robot_vel;
     counter2 = 10;
     key = 'e';
 
     // Starting message
-    printf("\n--- Manual Control ---\n\n"
-    "Linear velocity advised: 0.5\n"
-    "Angular velocity advised: 1.0\n");
+    std::cout << "--- Manual Control ---" << std::endl;
+    std::cout << "Linear velocity advised: 0.5" << std::endl;
+    std::cout << "Angular velocity advised: 1.0" << std::endl << std::endl;
 
     while (key != 'f') {
-        // Commands list
+        // Display commands list
         if (counter2 % 10 == 0) {
-            printf("\nCommands:\n"
-            "w - Go on\n"
-            "s - Go back\n"
-            "q - Curve left\n"
-            "e - Curve right\n"
-            "a - Turn left\n"
-            "d - Turn right\n"
-            "-----------------------------\n"
-            "z - Increase linear velocity\n"
-            "x - Decrease linear velocity\n"
-            "c - Increase angular velocity\n"
-            "v - Decrease angular velocity\n"
-            "-----------------------------\n"
-            "r - Emergency stop\n"
-            "f - Quit\n");
+            std::cout << "Commands:" << std::endl
+                      << "w - Go forward" << std::endl
+                      << "s - Go backward" << std::endl
+                      << "q - Curve left" << std::endl
+                      << "e - Curve right" << std::endl
+                      << "a - Turn left" << std::endl
+                      << "d - Turn right" << std::endl
+                      << "-----------------------------" << std::endl
+                      << "z - Increase linear velocity" << std::endl
+                      << "x - Decrease linear velocity" << std::endl
+                      << "c - Increase angular velocity" << std::endl
+                      << "v - Decrease angular velocity" << std::endl
+                      << "-----------------------------" << std::endl
+                      << "r - Emergency stop" << std::endl
+                      << "f - Quit" << std::endl;
         }
 
-        if (flag == 0) 
-            printf("h - Enable driving assistance\n");
+        if (flag == 0)
+            std::cout << "h - Enable driving assistance" << std::endl;
         else if (flag == 1)
-            printf("h - Disable driving assistance\n");
+            std::cout << "h - Disable driving assistance" << std::endl;
 
         // Take user input
-        printf("\nCommand: ");
+        std::cout << "Command: ";
         std::cin >> key;
 
         print_flag = 0;
 
-        if (key == 'w') { // Go on
-            robot_vel.linear.x = lin_vel;
-            robot_vel.angular.z = 0; 
-        }
-        else if (key == 'q') { // Curve left
-            robot_vel.linear.x = lin_vel;
-            robot_vel.angular.z = ang_vel; 
-        }
-        else if (key == 's') { // Go back
-            robot_vel.linear.x = -lin_vel;
-            robot_vel.angular.z = 0; 
-        }
-        else if (key == 'e') { // Curve right
-            robot_vel.linear.x = lin_vel;
-            robot_vel.angular.z = -ang_vel; 
-        }
-        else if (key == 'a') { // Turn left
-            robot_vel.linear.x = 0;
-            robot_vel.angular.z = ang_vel; 
-        }
-        else if (key == 'd') { // Turn right
-            robot_vel.linear.x = 0;
-            robot_vel.angular.z = -ang_vel; 
+        // Check if key is in command map
+        if(commandMap.count(key) > 0) {
+            robot_vel.linear.x = commandMap[key].first;
+            robot_vel.angular.z = commandMap[key].second;
         }
         else if (key == 'z') { // Increase linear velocity
-            lin_vel += 0.1;
+            lin_vel = std::min(lin_vel + 0.1f, 1.0f); // limit max velocity
         }
         else if (key == 'x') { // Decrease linear velocity
-            lin_vel -= 0.1;
+            lin_vel = std::max(lin_vel - 0.1f, 0.0f);
         }
-        else if (key == 'c') { // Increase angular velocity
-            ang_vel += 0.1;
-        }
-        else if (key == 'v') { // Decrease angular velocity
-            ang_vel -= 0.1;
-        }
-        else if (key == 'r') { // Emergency stop
-            robot_vel.linear.x = 0;
-            robot_vel.angular.z = 0; 
-        }
-        else if (key == 'h') { // Enable/Disable driving assistance
-            if (flag == 0) {
-                drive_flag = 1;
-                flag = 1;
-            }
-            else if (flag == 1) {
-                drive_flag = 0;
-                flag = 0;
-            }
-        }
-        else if (key == 'f') { // Quit
-            robot_vel.linear.x = 0;
-            robot_vel.angular.z = 0; 
-            pub_vel.publish(robot_vel);
-            counter1 = 10;
-            break;
-        }
-        
-        // Update message
-        printf("Linear velocity: %f\n"
-        "Angular velocity: %f\n", lin_vel, ang_vel);
-        pub_vel.publish(robot_vel);
-        counter2++;
     }
 }
 
-/**
- * ROS Callback Function: check data from robot's laser scanner and,
- * if the driving assistance is enable, help the user to not crush 
- * the robot against a wall. 
- * 
- * 
- */
+// Add function prototypes
+void findMinValues(const sensor_msgs::LaserScan::ConstPtr& msg, float& left, float& mid, float& right);
+void checkMaxTime();
+
 void drivingAssistance(const sensor_msgs::LaserScan::ConstPtr& msg) {
     // Local variables
     geometry_msgs::Twist robot_vel;
     float left = 30.0;
     float mid = 30.0;
     float right = 30.0;
-    int i;
-    
+
     // Take the minimum values
-    for (i = 0; i < 360; i++) { // On the right
-        if (msg->ranges[i] < right)
-            right = msg->ranges[i];
-    }
-    for (i = 300; i < 420; i++) { // In the middle
-        if (msg->ranges[i] < mid)
-            mid = msg->ranges[i];
-    }
-    for (i = 360; i < 720; i++) { // On the left
-        if (msg->ranges[i] < left)
-            left = msg->ranges[i];
-    }
+    findMinValues(msg, left, mid, right);
 
     // Driving assistance
     if (drive_flag == 1 & ((mid < DIST && key == 'w') || (left < DIST && key == 'q') || (right < DIST && key == 'e'))) {
@@ -191,6 +219,26 @@ void drivingAssistance(const sensor_msgs::LaserScan::ConstPtr& msg) {
     }
 
     // Check for the max time available to reach a goal point
+    checkMaxTime();
+}
+
+void findMinValues(const sensor_msgs::LaserScan::ConstPtr& msg, float& left, float& mid, float& right) {
+    int i;
+    for (i = 0; i < 360; i++) { // On the right
+        if (msg->ranges[i] < right)
+            right = msg->ranges[i];
+    }
+    for (i = 300; i < 420; i++) { // In the middle
+        if (msg->ranges[i] < mid)
+            mid = msg->ranges[i];
+    }
+    for (i = 360; i < 720; i++) { // On the left
+        if (msg->ranges[i] < left)
+            left = msg->ranges[i];
+    }
+}
+
+void checkMaxTime() {
     if (time_flag == 1) {
         t_end = std::chrono::high_resolution_clock::now();
         auto time = std::chrono::duration_cast<std::chrono::microseconds>(t_end - t_start).count();
@@ -202,16 +250,8 @@ void drivingAssistance(const sensor_msgs::LaserScan::ConstPtr& msg) {
             printf("Goal cancelled.\n");
             time_flag = 0;
         }
-    }   
+    }
 }
-
-/**
- * ROS Callback Function: check if the robot is on the goal position and,
- * when there is a new goal, update the current Goal ID and save its in a 
- * global variable.
- * 
- *  
- */
 void currentStatus(const move_base_msgs::MoveBaseActionFeedback::ConstPtr& msg) {
     // Take the current robot position
     float diff_x;
@@ -219,20 +259,21 @@ void currentStatus(const move_base_msgs::MoveBaseActionFeedback::ConstPtr& msg) 
     float current_x = msg->feedback.base_position.pose.position.x;
     float current_y = msg->feedback.base_position.pose.position.y;
 
+
     // Take the module
     if (current_x < 0)
         current_x *= -1;
     if (current_y < 0)
         current_y *= -1;
-    
+
     // Compute the error from the actual position and the goal position
     if (current_x >= x_goal)
         diff_x = current_x - x_goal;
-    else 
+    else
         diff_x = x_goal - current_x;
     if (current_y >= y_goal)
         diff_y = current_y - y_goal;
-    else 
+    else
         diff_y = y_goal - current_y;
 
     // The robot is on the goal position
@@ -247,12 +288,6 @@ void currentStatus(const move_base_msgs::MoveBaseActionFeedback::ConstPtr& msg) 
     }
 }
 
-/**
- * ROS Callback Function: check the current goal position and saves its 
- * module coordinates x and y in two global variables.
- * 
- * 
- */
 void currentGoal(const move_base_msgs::MoveBaseActionGoal::ConstPtr& msg) {
     x_goal = msg->goal.target_pose.pose.position.x;
     y_goal = msg->goal.target_pose.pose.position.y;
@@ -264,16 +299,6 @@ void currentGoal(const move_base_msgs::MoveBaseActionGoal::ConstPtr& msg) {
         y_goal *= -1;
 }
 
-/**
- * Function: provide an user interface to choose the modality to 
- * drive the robot; these are:
- * 1. Automatic driving (insert the coordinates to reach);
- *      1.1 it is also possible cancel the current goal.
- * 2. Manual driving (without driving assistance).
- * 3. Manual driving (with driving assistance);
- *      3.1 the driving assistance can be simply enabled or disabled.
- * 
- */
 void userInterface() {
     // Local variables
     move_base_msgs::MoveBaseActionGoal goal_pos;
@@ -286,22 +311,22 @@ void userInterface() {
         // Print commands list
         if (counter1 % 10 == 0) {
             printf("\nChoose an action:\n"
-            "0 - Exit\n"
-            "1 - Insert new coordinates to reach\n"
-            "2 - Cancel the current goal\n"
-            "3 - Manual driving\n");
+                   "0 - Exit\n"
+                   "1 - Insert new coordinates to reach\n"
+                   "2 - Cancel the current goal\n"
+                   "3 - Manual driving\n");
         }
-        if (flag == 0) 
+        if (flag == 0)
             printf("4 - Enable driving assistance\n");
         else if (flag == 1)
             printf("4 - Disable driving assistance\n");
-        
+
         // Take user input
         printf("Action (type the corresponding number): ");
         std::cin >> in;
 
         // Check input
-        if (in != '0' && in != '1' && in != '2' && in != '3' && in != '4') 
+        if (in != '0' && in != '1' && in != '2' && in != '3' && in != '4')
             printf("\nERROR: type '0', '1', '2', '3' or '4'.\n");
 
         counter1++;
@@ -315,7 +340,7 @@ void userInterface() {
 
         // Insert new coordinates to reach
         else if (in == '1') {
-            // Take coordinates to reach by the user
+            // Take coordinates to reach from the user
             printf("\nInsert coordinates to reach:\n");
             printf("X: ");
             std::cin >> X;
@@ -328,14 +353,14 @@ void userInterface() {
             // Set new coordinates to reach
             goal_pos.goal.target_pose.header.frame_id = "map";
             goal_pos.goal.target_pose.pose.orientation.w = 1;
-            
+
             goal_pos.goal.target_pose.pose.position.x = x;
             goal_pos.goal.target_pose.pose.position.y = y;
 
-            // Publish new goal 
+            // Publish new goal
             pub_goal.publish(goal_pos);
         }
-        
+
         // Cancel the current goal
         else if (in == '2') {
             canc_goal.id = id;
@@ -351,7 +376,7 @@ void userInterface() {
             manualDriving();
         }
 
-        // Enable or Disable drive assistance
+        // Enable or Disable driving assistance
         else if (in == '4') {
             if (flag == 0) {
                 drive_flag = 1;
@@ -365,32 +390,4 @@ void userInterface() {
             }
         }
     }
-}
-
-int main(int argc, char **argv) {
-
-
-    // Initialize the node
-    ros::init(argc, argv, "final_robot");
-    ros::NodeHandle nh;
-    
-    // Define the publishers
-    pub_goal = nh.advertise<move_base_msgs::MoveBaseActionGoal>("move_base/goal", 1000);
-    pub_canc = nh.advertise<actionlib_msgs::GoalID>("move_base/cancel", 1000);
-    pub_vel = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
-    
-    // Define the subscribers
-    ros::Subscriber sub_pos = nh.subscribe("/move_base/feedback", 1000, currentStatus); // Current  Status feedback
-    ros::Subscriber sub_goal = nh.subscribe("/move_base/goal", 1000, currentGoal); // Current Goal feedback
-    ros::Subscriber sub_laser = nh.subscribe("/scan", 1000, drivingAssistance); // Laser scanner
-
-    // Multi-threading
-    ros::AsyncSpinner spinner(3);
-    spinner.start();
-    userInterface();
-    spinner.stop();
-    ros::shutdown();
-    ros::waitForShutdown();
-
-    return 0;
 }
