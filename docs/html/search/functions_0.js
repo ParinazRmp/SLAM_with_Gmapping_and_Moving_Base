35d6fb8c3cc603716f242de9a6cdb6166af9c7ef
Parinaz <94115975+ParinazRmp@users.noreply.github.com>
2023-07-01 02:06:51 +0200

    Delete docs directory

--- docs/html/search/functions_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['currentgoal_12',['currentGoal',['../final__robot_8cpp.html#ae4710b8905b07ffceb0b3c89d11db805',1,'final_robot.cpp']]],
-  ['currentstatus_13',['currentStatus',['../final__robot_8cpp.html#abe5ed94ffa1a53d79c5a39fa0f0b1b31',1,'final_robot.cpp']]]
-];
